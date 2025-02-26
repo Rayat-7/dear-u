f4cc42239808fcb7719dab7eef46f53f873e86f9
Rayat-7 <raisoulrayat@gmail.com>
2025-02-27 04:23:30 +0600

    icon change

--- src/components/landingPage/Navbar.tsx
+++ src/components/landingPage/Navbar.tsx
@@ -1,10 +1,10 @@
-import { Home, User, Briefcase, FileText, Waypoints, MessageCircleCode, MessageCircleHeart, } from 'lucide-react'
+import { Home, Waypoints, MessageCircleCode, MessageCircleHeart, FileHeart, } from 'lucide-react'
 import { NavBar } from "@/components/ui/tubelight-navbar"
 import { motion } from 'framer-motion'
 export function NavBarDemo() {
   const navItems = [
     { name: 'Home', url: '#', icon: Home },
-    { name: 'Features', url: '#features', icon: MessageCircleHeart },
+    { name: 'Features', url: '#features', icon: FileHeart },
     { name: 'How It Works', url: '#how-it-works', icon: Waypoints },
     { name: 'FAQ', url: '#faq', icon: MessageCircleCode }
   ]
